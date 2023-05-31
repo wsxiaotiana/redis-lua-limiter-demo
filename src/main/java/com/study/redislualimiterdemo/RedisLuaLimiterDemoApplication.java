332d552aa833da52f6244e5c856504782558fac5
yanxiaotian <mark.yan@mvgx.com>
2023-05-31 09:39:59 +0800

    first commit

---
+++ src/main/java/com/study/redislualimiterdemo/RedisLuaLimiterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.study.redislualimiterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLuaLimiterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLuaLimiterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/redislualimiterdemo/RedisLuaLimiterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.redislualimiterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLuaLimiterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
